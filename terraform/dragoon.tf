acf55d2da13840fd6ba5acb56a05dc03c852cb3f
Protoss Probe <younghs+aft-aft@amazon.com>
2025-05-13 09:07:31 +0000

    Create Account Dragoon

---
+++ terraform/dragoon.tf
@@ -0,0 +1,27 @@
+module "dragoon" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "younghs+aft-dragoon@amazon.com"
+    AccountName               = "Protoss Dragoon"
+    ManagedOrganizationalUnit = "Protoss Gateway"
+    SSOUserEmail              = "younghs+aft-member@amazon.com"
+    SSOUserFirstName          = "Protoss"
+    SSOUserLastName           = "Member"
+  }
+
+  account_tags = {
+    "gateway" = "true"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Protoss Probe"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "gateway"
+  }
+
+  account_customizations_name = "gateway"
+}
